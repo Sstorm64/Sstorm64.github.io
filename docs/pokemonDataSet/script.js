167e056ad00754f39fa15690f070860315a762ef
Sstorm64 <samuelsiyuanzhao@gmail.com>
2026-03-14 22:09:16 -0400

    added BST calc

--- docs/pokemonDataSet/script.js
+++ docs/pokemonDataSet/script.js
@@ -7,9 +7,11 @@ fetch("pokedex.json")
   function displayPokemon(list){
   container.innerHTML = "";
   list.forEach(pokemon => {
-
+  sum = pokemon.base.Attack + pokemon.base.Defense + pokemon.base.HP+ pokemon.base["Sp. Attack"] + pokemon.base["Sp. Defense"] + pokemon.base.Speed;
   const div = document.createElement("div");
+ 
   div.innerHTML = `
+  
   <h3>#${pokemon.id} ${pokemon.name.english}</h3>
   <p>Type: ${pokemon.type.join(", ")}</p>
   <p>HP: ${pokemon.base.HP}</p>
@@ -18,6 +20,7 @@ fetch("pokedex.json")
   <p>Sp. Attack: ${pokemon.base["Sp. Attack"]}</p>
   <p>Sp. Defense: ${pokemon.base["Sp. Defense"]}</p>
   <p>Speed ${pokemon.base.Speed}</p>
+  <p>BST ${sum}</p>
   <hr>
     `;
 
