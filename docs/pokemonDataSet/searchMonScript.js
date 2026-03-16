8c4d4ef7aa3c2e6c1d17ab714eb4643cc84db2cf
Sstorm64 <samuelsiyuanzhao@gmail.com>
2026-03-16 00:06:58 -0400

    comments

--- docs/pokemonDataSet/searchMonScript.js
+++ docs/pokemonDataSet/searchMonScript.js
@@ -1,30 +1,31 @@
-fetch("pokedex.json")
+fetch("pokedex.json") /*takes all the data from pokdex.json file, converts it to an array that can be easily parsed*/
     .then(res => res.json())
     .then(data => {
-const container = document.getElementById("pokemonList")
-data.forEach(pokemon=>{
-let row = document.createElement("div")
-row.className = "pokemonRow"
-row.innerHTML =`#${pokemon.id}
+        const pokdexListContainer = document.getElementById("pokemonList") /*connects the pokemonList element to a list/array/box that we could add stuff to */
+        data.forEach(pokemon => {
+            let pokemonEntry = document.createElement("div") /*Creates a div that is a class called PokemonEntry it contains the entries of the pokemon from the JSON*/
+            pokemonEntry.className = "pokemonEntry" /*adds the class to the said div as pokemon Entry */
+            pokemonEntry.innerHTML = `#${pokemon.id} 
 <img src="img/${pokemon.id}.png">
 <a href ="info.html?id=${pokemon.id}">
     ${pokemon.name.english}
 </a>
-`;
-container.appendChild(row);
-});
-    document.getElementById("search").addEventListener("input",function(){
-    let filter = this.value.toLowerCase()
-  
-    let rows = document.querySelectorAll(".pokemonRow")
-    rows.forEach(row=>{
-if(row.textContent.toLowerCase().includes(filter)){
-    row.style.display="";
-}
-else{
-    row.style.display="none";
-}
-    });
-      });
+`; /*adds the actual info into the newly created pokemon Entry, the Div class we just created  */
+            pokdexListContainer.appendChild(pokemonEntry);
+        });
+        document.getElementById("search").addEventListener("input", function() {
+            /*checks search bar element as checks it everytime something is added or removed then sends the value to UserInput*/
+            let userInput = this.value.toLowerCase()
 
-});
+            let pokemonList = document.querySelectorAll(".pokemonEntry")
+            pokemonList.forEach(Entry => { /*Scans the list of pokemon and puts each one into the Entry varible which is then further used */
+                if (Entry.textContent.toLowerCase().includes(userInput)) {
+                    /*checks if what the user inputted is included in the pokemon's name, then displays similar entries or displays nothing */
+                    Entry.style.display = "";
+                } else {
+                    Entry.style.display = "none";
+                }
+            });
+        });
+
+    });
